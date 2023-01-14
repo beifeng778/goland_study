93b04e89455af1f2f5ebfcd7d1dc241737086432
rainyday <992864928@qq.com>
2023-01-15 01:40:50 +0800

    init version 1.0

---
+++ com.rain/main/test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"golang_day1/com.rain/hello"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(time.Now())
+	hello.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang_day1
+
+go 1.19
